21feed7019857148e835fe8cc0968f75c88a87a1
Enmanuel <65689762+ysEnmanuel@users.noreply.github.com>
2021-02-15 23:33:40 -0500

    Mod V3.7

--- assets/js/wb.js
+++ assets/js/wb.js
@@ -1,9 +1,9 @@
 $(function() {
   $('#WAButton').floatingWhatsApp({
-    phone: '', //WhatsApp Business phone number International format-
+    phone: '51999934443', //WhatsApp Business phone number International format-
     //Get it with Toky at https://toky.co/en/features/whatsapp.
     headerTitle: 'Chatea Por Whatsapp!', //Popup Title
-    popupMessage: 'Hola,como lo puedo ayudar?', //Popup Message
+    popupMessage: 'Hola,como lo puedo?', //Popup Message
     showPopup: true, //Enables popup display
     buttonImage: '<img src="https://rawcdn.githack.com/rafaelbotazini/floating-whatsapp/3d18b26d5c7d430a1ab0b664f8ca6b69014aed68/whatsapp.svg" />', //Button Image
     //headerColor: 'crimson', //Custom header color
