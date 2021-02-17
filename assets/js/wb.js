0077b21b8006120a790ca04c3c364236299ff025
Enmanuel <65689762+ysEnmanuel@users.noreply.github.com>
2021-02-16 21:25:44 -0500

    Mod V3.9

--- assets/js/wb.js
+++ assets/js/wb.js
@@ -1,13 +1,10 @@
 $(function() {
   $('#WAButton').floatingWhatsApp({
-    phone: '51999934443', //WhatsApp Business phone number International format-
-    //Get it with Toky at https://toky.co/en/features/whatsapp.
-    headerTitle: 'Chatea Por Whatsapp!', //Popup Title
-    popupMessage: 'Hola,como lo puedo ayudar?', //Popup Message
-    showPopup: true, //Enables popup display
-    buttonImage: '<img src="https://rawcdn.githack.com/rafaelbotazini/floating-whatsapp/3d18b26d5c7d430a1ab0b664f8ca6b69014aed68/whatsapp.svg" />', //Button Image
-    //headerColor: 'crimson', //Custom header color
-    //backgroundColor: 'crimson', //Custom background button color
+    phone: '51999934443',
+    headerTitle: 'Chatea Por Whatsapp!',
+    popupMessage: 'Hola,como lo puedo ayudar?',
+    showPopup: true,
+    buttonImage: '<img src="https://rawcdn.githack.com/rafaelbotazini/floating-whatsapp/3d18b26d5c7d430a1ab0b664f8ca6b69014aed68/whatsapp.svg" />',
     position: "left"    
   });
 });
