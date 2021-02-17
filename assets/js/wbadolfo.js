a5c62ae40cea3af80eae4fd987562e3f6d2374e4
Enmanuel <65689762+ysEnmanuel@users.noreply.github.com>
2021-02-16 21:53:52 -0500

    Mod V4.2

---
+++ assets/js/wbadolfo.js
@@ -0,0 +1,10 @@
+$(function() {
+  $('#WAButton').floatingWhatsApp({
+    phone: '51936898339',
+    headerTitle: 'Chatea Por Whatsapp!',
+    popupMessage: 'Hola,como lo puedo ayudar?',
+    showPopup: true,
+    buttonImage: '<img src="https://rawcdn.githack.com/rafaelbotazini/floating-whatsapp/3d18b26d5c7d430a1ab0b664f8ca6b69014aed68/whatsapp.svg" />',
+    position: "left"    
+  });
+});
