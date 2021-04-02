bfadf3e4baf546d280d6d9dbb009b7bc5c24a104
Enmanuel <cristopherariaspacheco@gmail.com>
2021-04-01 21:05:39 -0500

    Mod V7.3

--- assets/js/wb.js
+++ assets/js/wb.js
@@ -4,7 +4,7 @@ $(function() {
     headerTitle: 'Chatea Por Whatsapp!',
     popupMessage: 'Hola,como lo puedo ayudar?',
     showPopup: true,
-    buttonImage: '<img src="https://rawcdn.githack.com/rafaelbotazini/floating-whatsapp/3d18b26d5c7d430a1ab0b664f8ca6b69014aed68/whatsapp.svg" />',
+    buttonImage: '<img alt="Boton Whatsapp" src="assets/img/whatsapp.svg"/>',
     position: "left"    
   });
 });
