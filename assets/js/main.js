9b16a206fa19db4fc8cc69b0a7e693e0cd109e55
Enmanuel <cristopherariaspacheco@gmail.com>
2021-04-02 21:09:55 -0500

    Mod V7.9

--- assets/js/main.js
+++ assets/js/main.js
@@ -61,12 +61,12 @@
       class: 'mobile-nav d-lg-none'
     });
     $('body').append($mobile_nav);
-    $('body').prepend('<button type="button" class="mobile-nav-toggle d-lg-none"><i class="icofont-navigation-menu"></i></button>');
+    $('body').prepend('<button type="menu" class="mobile-nav-toggle d-lg-none"><i class="far fa-bars"></i></button>');
     $('body').append('<div class="mobile-nav-overly"></div>');
 
     $(document).on('click', '.mobile-nav-toggle', function (e) {
       $('body').toggleClass('mobile-nav-active');
-      $('.mobile-nav-toggle i').toggleClass('icofont-navigation-menu icofont-close');
+      $('.mobile-nav-toggle i').toggleClass('far fa-bars far fa-times');
       $('.mobile-nav-overly').toggle();
     });
 
@@ -81,7 +81,7 @@
       if (!container.is(e.target) && container.has(e.target).length === 0) {
         if ($('body').hasClass('mobile-nav-active')) {
           $('body').removeClass('mobile-nav-active');
-          $('.mobile-nav-toggle i').toggleClass('icofont-navigation-menu icofont-close');
+          $('.mobile-nav-toggle i').toggleClass('far fa-bars far fa-times');
           $('.mobile-nav-overly').fadeOut();
         }
       }
