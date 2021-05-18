134fa17f150d185ff3123fcb278e6cb334e60246
Enmanuel <cristopherariaspacheco@gmail.com>
2021-05-18 00:48:59 -0500

    Mod V8.8
    :O

---
+++ assets/js/scrollreveal.js
@@ -0,0 +1,71 @@
+window.sr = ScrollReveal();
+  sr.reveal('.info1', {
+    duration: 3500,
+    origin: 'right',
+    distance: '-100px'
+  })
+  sr.reveal('.info', {
+    duration: 3500,
+    origin: 'left',
+    distance: '-100px'
+  })
+  sr.reveal('.team', {
+    duration: 3500,
+    origin: 'right',
+    distance: '-100px'
+  })
+  sr.reveal('.raw', {
+    duration: 3500,
+    origin: 'left',
+    distance: '-100px'
+  })
+  sr.reveal('header', {
+    duration: 1000,
+    origin: 'bottom',
+    distance: '-100px'
+  })
+  sr.reveal('.hero', {
+    duration: 2000,
+    origin: 'bottom',
+    distance: '-100px'
+  })
+  sr.reveal('.counts', {
+    duration: 3500,
+    origin: 'up',
+    distance: '-200px'
+  })
+  sr.reveal('.testimonials', {
+    duration: 3500,
+    origin: 'bottom',
+    distance: '-100px'
+  })
+  sr.reveal('.row', {
+    duration: 3500,
+    origin: 'top',
+    distance: '-100px'
+  })
+  sr.reveal('.services', {
+    duration: 3500,
+    origin: 'bottom',
+    distance: '-100px'
+  })
+  sr.reveal('footer', {
+    duration: 3500,
+    origin: 'top',
+    distance: '-100px'
+  })
+//   sr.reveal('.services', {
+//     duration: 2000,
+//     origin: 'top',
+//     distance: '-50px'
+//   })
+//   sr.reveal('.services', {
+//     duration: 2000,
+//     origin: 'top',
+//     distance: '-50px'
+//   })
+//   sr.reveal('.services', {
+//     duration: 2000,
+//     origin: 'top',
+//     distance: '-50px'
+//   });
---
+++ assets/js/smoothscroll.js
@@ -0,0 +1,7 @@
+import Scrollbar from "smooth-scrollbar";
+
+var options = {
+    "damping":1
+}
+
+Scrollbar.init(document.querySelector("#my-scrollbar"), options);
